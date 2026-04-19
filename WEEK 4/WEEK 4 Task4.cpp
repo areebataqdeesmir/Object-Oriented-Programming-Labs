9506628b9e8437dfc9cd6cb74f780da610822f91
areebataqdeesmir <areebataqdees07@gmail.com>
2026-04-19 12:07:14 +0500

    Add files via upload

---
+++ WEEK 4/WEEK 4 Task4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Printer {
+public:
+    void print_document() {
+        cout << "Printing document..." << endl;
+    }
+};
+
+class Scanner {
+public:
+    void scan_document() {
+        cout << "Scanning document..." << endl;
+    }
+};
+
+class Photocopier : public Printer, public Scanner {
+public:
+    void photocopy() {
+        cout << "Photocopying..." << endl;
+    }
+};
+
+int main() {
+    Photocopier p;
+
+    p.print_document();
+    p.scan_document();
+    p.photocopy();
+
+    return 0;
+}
